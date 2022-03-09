379041ced460328d900bb9b3b216387187698ed7
mtkarimi97 <mtkarimi97@gmail.com>
2022-03-09 17:29:37 -0500

    Finished test for PrayerDay.kt, added test for Utils.kt

--- app/src/test/java/org/raleighmasjid/iar/PrayerDayTest.kt
+++ app/src/test/java/org/raleighmasjid/iar/PrayerDayTest.kt
@@ -16,14 +16,14 @@ class PrayerDayTest {
 
         val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
 
-        // Adhan time for 'asr for February 1st, 2022 is 2:45 PM
-        val testAdhanDateTimeString = "2022-02-01T15:21:00-05:00"
-        val testIqamahDateTimeString = "2022-02-01T16:00:00-05:00"
+        // Adhan time for 'asr for February 1st, 2022 is 3:21 PM
+        val correctAdhanDateTimeString = "2022-02-01T15:21:00-05:00"
+        val correctIqamahDateTimeString = "2022-02-01T16:00:00-05:00"
         val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
-        val testAdhanTime = formatter.parse(testAdhanDateTimeString) // This should say:  Feb 01 15:21:00 EST 2022
-        val testIqamahTime = formatter.parse(testIqamahDateTimeString)
+        val correctAdhanTime = formatter.parse(correctAdhanDateTimeString) // This should say:  Feb 01 15:21:00 EST 2022
+        val correctIqamahTime = formatter.parse(correctIqamahDateTimeString)
         val prayer = Prayer.ASR
-        val testPrayerTime = PrayerTime(prayer, testAdhanTime!!, testIqamahTime) // the prayer and
+        val correctPrayerTime = PrayerTime(prayer, correctAdhanTime!!, correctIqamahTime) // the prayer and
         // adhan from the upcoming prayer method should match this
 
         // Creating a fake 'current' time to pass in for it to see what the upcoming prayer time is
@@ -33,10 +33,10 @@ class PrayerDayTest {
         val upcomingPrayerTime = PrayerDay.upcomingPrayer(prayerDays!!, testCurrentTime!!)
 
         if (upcomingPrayerTime != null) {
-            Assert.assertEquals(testPrayerTime.prayer, upcomingPrayerTime.prayer)
-            Assert.assertEquals(testPrayerTime.adhan, upcomingPrayerTime.adhan)
-            Assert.assertEquals(testPrayerTime.iqamah, upcomingPrayerTime.iqamah)
-            Assert.assertEquals(testPrayerTime.notificationTime(), upcomingPrayerTime.notificationTime())
+            Assert.assertEquals(correctPrayerTime.prayer, upcomingPrayerTime.prayer)
+            Assert.assertEquals(correctPrayerTime.adhan, upcomingPrayerTime.adhan)
+            Assert.assertEquals(correctPrayerTime.iqamah, upcomingPrayerTime.iqamah)
+            Assert.assertEquals(correctPrayerTime.notificationTime(), upcomingPrayerTime.notificationTime())
         }
     }
 
@@ -45,14 +45,14 @@ class PrayerDayTest {
 
         val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
 
-        // Adhan time for 'asr for February 1st, 2022 is 2:45 PM
-        val testAdhanDateTimeString = "2022-02-01T05:48:00-05:00"
-        val testIqamahDateTimeString = "2022-02-01T06:15:00-05:00"
+        // Adhan time for fajr for February 1st, 2022 is 5:48 AM
+        val correctAdhanDateTimeString = "2022-02-01T05:48:00-05:00"
+        val correctIqamahDateTimeString = "2022-02-01T06:15:00-05:00"
         val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
-        val testAdhanTime = formatter.parse(testAdhanDateTimeString) // This should say:  Feb 01 15:21:00 EST 2022
-        val testIqamahTime = formatter.parse(testIqamahDateTimeString)
+        val correctAdhanTime = formatter.parse(correctAdhanDateTimeString) // This should say:  Feb 01 15:21:00 EST 2022
+        val correctIqamahTime = formatter.parse(correctIqamahDateTimeString)
         val prayer = Prayer.FAJR
-        val testPrayerTime = PrayerTime(prayer, testAdhanTime!!, testIqamahTime) // the prayer and
+        val correctPrayerTime = PrayerTime(prayer, correctAdhanTime!!, correctIqamahTime) // the prayer and
         // adhan from the upcoming prayer method should match this
 
         // Creating a fake 'current' time to pass in for it to see what the upcoming prayer time is
@@ -62,11 +62,171 @@ class PrayerDayTest {
         val upcomingPrayerTime = PrayerDay.upcomingPrayer(prayerDays!!, testCurrentTime!!)
 
         if (upcomingPrayerTime != null) {
-            Assert.assertEquals(testPrayerTime.prayer, upcomingPrayerTime.prayer)
-            Assert.assertEquals(testPrayerTime.adhan, upcomingPrayerTime.adhan)
-            Assert.assertEquals(testPrayerTime.iqamah, upcomingPrayerTime.iqamah)
-            Assert.assertEquals(testPrayerTime.notificationTime(), upcomingPrayerTime.notificationTime())
+            Assert.assertEquals(correctPrayerTime.prayer, upcomingPrayerTime.prayer)
+            Assert.assertEquals(correctPrayerTime.adhan, upcomingPrayerTime.adhan)
+            Assert.assertEquals(correctPrayerTime.iqamah, upcomingPrayerTime.iqamah)
+            Assert.assertEquals(correctPrayerTime.notificationTime(), upcomingPrayerTime.notificationTime())
         }
+    }
+
+    @Test
+    fun upcomingPrayerBeforeTestJSON() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Adhan time for fajr for Jan 31st, 2022 is 5:48 AM
+        val correctAdhanDateTimeString = "2022-01-31T05:48:00-05:00"
+        val correctIqamahDateTimeString = "2022-01-31T06:15:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val correctAdhanTime = formatter.parse(correctAdhanDateTimeString) // This should say:  Feb 01 15:21:00 EST 2022
+        val correctIqamahTime = formatter.parse(correctIqamahDateTimeString)
+        val prayer = Prayer.FAJR
+        val correctPrayerTime = PrayerTime(prayer, correctAdhanTime!!, correctIqamahTime) // the prayer and
+        // adhan from the upcoming prayer method should match this
+
+        // Creating a fake 'current' time to pass in for it to see what the upcoming prayer time is
+        val currentDateTimeString = "2022-01-28T12:00:00-05:00"
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+
+        val upcomingPrayerTime = PrayerDay.upcomingPrayer(prayerDays!!, testCurrentTime!!)
+
+        if (upcomingPrayerTime != null) {
+            Assert.assertEquals(correctPrayerTime.prayer, upcomingPrayerTime.prayer)
+            Assert.assertEquals(correctPrayerTime.adhan, upcomingPrayerTime.adhan)
+            Assert.assertEquals(correctPrayerTime.iqamah, upcomingPrayerTime.iqamah)
+            Assert.assertEquals(correctPrayerTime.notificationTime(), upcomingPrayerTime.notificationTime())
+        }
+    }
+
+    @Test
+    fun upcomingPrayerAfterTestJSON() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // return value of function upcomingPrayer should be null, but just in case will init these
+        val correctAdhanDateTimeString = "2022-01-31T05:48:00-05:00"
+        val correctIqamahDateTimeString = "2022-01-31T06:15:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val correctAdhanTime = formatter.parse(correctAdhanDateTimeString) // This should say:  Feb 01 15:21:00 EST 2022
+        val correctIqamahTime = formatter.parse(correctIqamahDateTimeString)
+        val prayer = Prayer.FAJR
+        val correctPrayerTime = PrayerTime(prayer, correctAdhanTime!!, correctIqamahTime) // the prayer and
+        // adhan from the upcoming prayer method should match this
+
+        // Creating a fake 'current' time to pass in for it to see what the upcoming prayer time is
+        val currentDateTimeString = "2022-02-10T12:00:00-05:00"
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+
+        val upcomingPrayerTime = PrayerDay.upcomingPrayer(prayerDays!!, testCurrentTime!!)
+
+        if (upcomingPrayerTime != null) {
+            Assert.assertEquals(correctPrayerTime.prayer, upcomingPrayerTime.prayer)
+            Assert.assertEquals(correctPrayerTime.adhan, upcomingPrayerTime.adhan)
+            Assert.assertEquals(correctPrayerTime.iqamah, upcomingPrayerTime.iqamah)
+            Assert.assertEquals(correctPrayerTime.notificationTime(), upcomingPrayerTime.notificationTime())
+        } else {
+            Assert.assertNull(upcomingPrayerTime)
+        }
+    }
+
+    @Test
+    fun currentPrayerIshaSameDay() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T20:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+        val correctPrayer = Prayer.ISHA
+
+        Assert.assertEquals(correctPrayer, prayerDays!![0].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerIshaDiffDay() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T20:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+
+        Assert.assertNull(prayerDays!![1].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerMaghrib() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T18:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+        val correctPrayer = Prayer.MAGHRIB
+
+        Assert.assertEquals(correctPrayer, prayerDays!![0].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerAsr() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T16:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+        val correctPrayer = Prayer.ASR
+
+        Assert.assertEquals(correctPrayer, prayerDays!![0].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerDhuhr() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T13:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+        val correctPrayer = Prayer.DHUHR
+
+        Assert.assertEquals(correctPrayer, prayerDays!![0].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerShuruq() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T09:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+        val correctPrayer = Prayer.SHURUQ
+
+        Assert.assertEquals(correctPrayer, prayerDays!![0].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerFajr() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T06:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+        val correctPrayer = Prayer.FAJR
+
+        Assert.assertEquals(correctPrayer, prayerDays!![0].currentPrayer(testCurrentTime!!))
+    }
+
+    @Test
+    fun currentPrayerBeforeFajr() {
+        val prayerDays = ApiClient.moshi.decodeList<PrayerDay>(testJSON)
+
+        // Creating a fake 'current' time to pass in for it to see what the current prayer time is
+        val currentDateTimeString = "2022-01-31T04:00:00-05:00"
+        val formatter = SimpleDateFormat("y-L-d'T'HH:mm:ssX")
+        val testCurrentTime = formatter.parse(currentDateTimeString)
+
+        Assert.assertNull(prayerDays!![0].currentPrayer(testCurrentTime!!))
     }
 
     private val testJSON =
