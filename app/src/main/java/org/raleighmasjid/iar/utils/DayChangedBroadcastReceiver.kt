aca4b46de783d44a2f14f5eced0b132a4f5f0519
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2022-03-18 16:08:14 -0400

    small cleanup

--- app/src/main/java/org/raleighmasjid/iar/utils/DayChangedBroadcastReceiver.kt
+++ app/src/main/java/org/raleighmasjid/iar/utils/DayChangedBroadcastReceiver.kt
@@ -4,24 +4,20 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import java.text.SimpleDateFormat
-import java.util.*
 
 abstract class DayChangedBroadcastReceiver : BroadcastReceiver() {
-
-    override fun onReceive(context: Context, intent: Intent) {
-        val action = intent.action
-
-        if ((action == Intent.ACTION_DATE_CHANGED)) {
-            onDayChanged()
-        }
-    }
-
-    abstract fun onDayChanged()
 
     companion object {
         fun getIntentFilter() = IntentFilter().apply {
             addAction(Intent.ACTION_DATE_CHANGED)
         }
     }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_DATE_CHANGED) {
+            onDayChanged()
+        }
+    }
+
+    abstract fun onDayChanged()
 }
