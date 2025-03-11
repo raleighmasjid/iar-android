029aef2fcebaead3b491b3783a12079cf7754fb4
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2025-03-11 00:07:45 -0400

    migrated gradle files to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.jetbrains.kotlin.compose)
+}
+
+android {
+    namespace = "com.madinaapps.iarmasjid"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.madinaapps.iarmasjid"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 225
+        versionName = "3.1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            ndk {
+                debugSymbolLevel = "full"
+            }
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.11"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.work.ktx)
+    implementation(libs.onesignal)
+    implementation(libs.core.ktx)
+    implementation(libs.bundles.compose)
+    implementation(libs.navigation.compose)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.activity.compose)
+    implementation(libs.bundles.accompanist)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.moshi)
+    implementation(libs.moshi.adapters)
+    implementation(libs.datastore.preferences)
+    implementation(libs.dagger.hilt)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.coil.compose)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.moshi.kotlin.codegen)
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.compose.test.junit4)
+    debugImplementation(libs.compose.ui.tooling)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    dependencies {
+        classpath(libs.hilt.android.gradle.plugin)
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.jetbrains.kotlin.android).apply(false)
+    alias(libs.plugins.ksp).apply(false)
+    alias(libs.plugins.jetbrains.kotlin.compose).apply(false)
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
